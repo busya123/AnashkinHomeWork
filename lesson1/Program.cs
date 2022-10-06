bcd1038a5f60773978632c1bf7b71bb05aa92640
Jaroslav <you@example.com>
2022-10-06 12:07:34 +0300

    HomeWorke

---
+++ lesson1/Program.cs
@@ -0,0 +1,146 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+/*
+int A = 5;
+int B = 7;
+int max = A;
+int min = A;
+
+if(A > max) max = A;
+if(B > max) max = B;
+if(A < min) min = A;
+if(B < min) min = B;
+
+
+    Console.WriteLine(max );
+    Console.WriteLine(min );
+*/
+
+
+/*
+int A = 2;
+int B = 10;
+int max = A;
+int min = A;
+
+if(A > max) max = A;
+if(B > max) max = B;
+if(A < min) min = A;
+if(B < min) min = B;
+
+
+    Console.WriteLine(max);
+    Console.WriteLine(min);
+*/
+
+
+/*
+int A = -9;
+int B = -3;
+int max = A;
+int min = A;
+
+if(A > max) max = A;
+if(B > max) max = B;
+if(A < min) min = A;
+if(B < min) min = B;
+
+   Console.WriteLine(max );
+   Console.WriteLine(min );
+*/    
+
+
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+/*
+int a = 2;
+int b = 3;
+int c = 7;
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max );
+*/
+/*
+int a = 44;
+int b = 5;
+int c = 78;
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max );
+*/
+/*
+int a = 22;
+int b = 3;
+int c = 9;
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max );
+*/
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+/*
+int x = 4 ;
+//x =  Console.Read();
+if ((x % 2) == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
+*/
+ /*      
+int x = -3 ;
+//x =  Console.Read();
+if ((x % 2) == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
+  */     
+    /*
+int x = 7 ;
+//x =  Console.Read();
+if ((x % 2) == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
+    */
+  //Напишите программу которая на вход принимает число nа на выходе показывает все чётные числа от 1 до n
+
+    /*
+ for (int x = 1; x <= 5; x++)       
+if (x%2==0)            
+Console.WriteLine(x);            
+*/
+
+
+ for (int x = 1; x <= 8; x++)       
+if (x%2==0)            
+Console.WriteLine(x);
+
